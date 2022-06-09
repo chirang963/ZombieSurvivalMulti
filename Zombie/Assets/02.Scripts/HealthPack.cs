c8848ca0042ffb8ded87d484623164d338e5a46c
chirang963 <qawsedrf963@naver.com>
2022-06-09 16:39:39 +0900

    06.09.005
    06.09.005

--- Zombie/Assets/02.Scripts/HealthPack.cs
+++ Zombie/Assets/02.Scripts/HealthPack.cs
@@ -1,21 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
-public class HealthPack : MonoBehaviour, IItem
+public class HealthPack : MonoBehaviourPun, IItem
 {
-    public int health = 50; // 체력을 회복할 수치
+    public float health = 50;
 
     public void Use(GameObject target)
     {
+        // 전달받은 게임 오브젝트로부터 LivingEntity 컴포넌트
+        // 가져오기 시도
         LivingEntity life = target.GetComponent<LivingEntity>();
 
-        if (life != null)
+        // LivingEntity 컴포넌트가 있다면
+        if(life != null)
         {
+            // 체력 회복 실행
             life.RestoreHealth(health);
         }
 
-        Destroy(gameObject);
+        // 사용되었으므로 자신을 파괴 -> 모든 클라이언트에서 자신 파괴
+        // Destroy(gameObject);
+        PhotonNetwork.Destroy(gameObject);
     }
-
 }
--- Zombie/Assets/02.Scripts/ItemSpawner.cs
+++ Zombie/Assets/02.Scripts/ItemSpawner.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.AI; // 내비메시 관련 코드
 
@@ -5,7 +7,7 @@ using UnityEngine.AI; // 내비메시 관련 코드
 public class ItemSpawner : MonoBehaviour
 {
     public GameObject[] items; // 생성할 아이템
-    public Transform playerTransform; // 플레이어의 트랜스폼
+
 
     // 플레이어 위치에서 아이템이 배치될 최대 반경
     public float maxDistance = 5f;
@@ -26,9 +28,12 @@ public class ItemSpawner : MonoBehaviour
     // 주기적으로 아이템 생성 처리 실행
     void Update()
     {
-        // 현재 시점이 마지막 생성 시점에서 생성 주기 이상 지남
-        // &&(이면서) 플레이어 캐릭터가 존재함
-        if (Time.time >= lastSpawnTime + timeBetSpawn && playerTransform != null)
+        // 호스트에서만 아이템 직접 생성 가능
+        if(!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+        if (Time.time >= lastSpawnTime + timeBetSpawn)
         {
             // 마지막 생성 시간 갱신
             lastSpawnTime = Time.time;
@@ -42,21 +47,29 @@ public class ItemSpawner : MonoBehaviour
     // 실제 아이템 생성 처리
     private void Spawn()
     {
-        // 플레이어 근처에서 내비메시 위의 랜덤 위치 가져오기
-        Vector3 spawnPosition = GetRandomPointOnNavMesh(
-            playerTransform.position, maxDistance);
+        // (0,0,0)을 기준으로 maxDistance 안에서 내비메시 위의 랜덤 위치 지정
+        Vector3 spawnPosition = GetRandomPointOnNavMesh(Vector3.zero, maxDistance);
         // 바닥에서 0.5만큼 위로 올리기
         spawnPosition += Vector3.up * 0.5f;
 
-        // 아이템 중 하나를 무작위로 골라 랜덤 위치에 생성
+        // 생성할 아이템을 무작위로 하나 선택
         GameObject selectedItem = items[Random.Range(0, items.Length)];
-        GameObject item = Instantiate(selectedItem, spawnPosition,
-        Quaternion.identity);
+        GameObject item = PhotonNetwork.Instantiate(selectedItem.name, spawnPosition,Quaternion.identity);
 
         // 생성된 아이템을 5초 뒤에 파괴
-        Destroy(item, 5f);
+        StartCoroutine(DestroyAfter(item, 5f));
     }
-
+    // 포톤의 PhotonNetwork.Destroy()를 지연 실행하는 코루틴
+    IEnumerator DestroyAfter(GameObject target, float delay)
+    {   //delay만큼 대기
+        yield return new WaitForSeconds(delay);
+    
+        //target이 파괴되지 않았다면 파괴 실행
+        if(target !=null)
+        {
+            PhotonNetwork.Destroy(target);
+        }
+    }
     // 내비메시 위의 랜덤한 위치를 반환하는 메서드
     // center를 중심으로 distance 반경 안에서의 랜덤한 위치를 찾음
     private Vector3 GetRandomPointOnNavMesh(Vector3 center, float distance)
